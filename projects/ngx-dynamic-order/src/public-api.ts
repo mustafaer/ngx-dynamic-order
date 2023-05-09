4c51f2e3e7cf0998cf1d184102a5cf9ea284430b
Mustafa ER <mustafaerpro@gmail.com>
2023-05-09 08:38:22 +0300

    order pipe created

--- projects/ngx-dynamic-order/src/public-api.ts
+++ projects/ngx-dynamic-order/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-dynamic-order
  */
 
-export * from './lib/ngx-dynamic-order.service';
-export * from './lib/ngx-dynamic-order.component';
-export * from './lib/ngx-dynamic-order.module';
+export * from './lib/ngx-dynamic-order.pipe';
